bb5725e171c98ad0bd32f566a0eddcf6cf600f05
alvaro779-del <generalauxilio@gmail.com>
2025-04-06 23:47:14 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,4 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("mayor-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "index.html",
-        "estilo.css",
-        "script.js",
-        "manifesto.json",
-        "icono.png",
-        "avatar-mayor.png",
-        "sello-imperial.png"
-      ]);
-    })
-  );
+self.addEventListener("install", function(event) {
+  console.log("Service Worker instalado.");
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
-});
